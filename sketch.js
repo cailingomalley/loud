f61c4cc43b3b03944cafab4ad09033c44bdaa2aa
cailingomalley <92131682+cailingomalley@users.noreply.github.com>
2022-10-20 11:44:25 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+
+function setup() {
+  createCanvas(1000, 1000);
+}
+function draw() {
+//background(260);
+//image(creepy, 0,0,1000,1000);
+//tint(255,255);
+//image(twoface,0,300,700,700);
+//tint(255, 127)
+}
